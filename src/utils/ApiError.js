dcc31c1da022e6b7c3f582b204e336cb17a5a76c
vaibhva0925 <vaibha0925@gmail.com>
2024-03-11 19:33:47 +0530

    utils core discusions

---
+++ src/utils/ApiError.js
@@ -0,0 +1,24 @@
+const { Error } = require("mongoose")
+
+class  ApiError extends Error {
+    constructor(
+        statuscode,
+        message= "Something went wrong",
+        errors = [],
+        stack = ""
+    ){
+        super(message),
+        this.statusCode = statuscode
+        this.data=null
+        this.message=message
+        this.success=false
+        this.errors=errors
+
+        if (statck) {
+            this.stack=statck
+        } else{
+            Error.captureStackTrace(this, this.constructor)
+        }
+    }
+}
+export {ApiError}
--- src/utils/AsyncHandeler.js
+++ src/utils/AsyncHandeler.js
@@ -1,9 +1,27 @@
-//const asyncHandelaer = () => {}
+import { Promise } from "mongoose"
+
+const asyncHandelaer = (reqestHandeler) => {
+    (req, res, next) => {
+        Promise.resolve(reqestHandeler(req, res, next)).catch((err) => next(err))
+    }
+}
 
 
 
 export {asyncHandelaer}
 
 
+// const asyncHolder =  () => {}
+// const asyncHolder = (func) => () => {}
+// const asyncHolder = (func) => async() => {}
 
-const asyncHandelaer = (fn) => async() => {}
+// const asyncHandelaer = (fn) => async(req, res, next) => {
+//     try {
+//         await fn(req, res, next);
+//     } catch (error) {
+//         res.status(err. code || 500).jso({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
